7d0297eeaf099228661c587c8e7ad9e43d7c1c76
Adrian Calabro <calabroadrian@gmail.com>
2023-08-03 09:52:59 -0300

    cerrar sesion

--- src/components/AuthContext/AuthContext.js
+++ src/components/AuthContext/AuthContext.js
@@ -1,29 +1,30 @@
-import { createContext, useState, useContext } from 'react';
+    import { createContext, useState, useContext } from 'react';
 
-const AuthContext = createContext();
+    const AuthContext = createContext();
 
-export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
+    export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
 
-  // Función para iniciar sesión (puedes modificarla según tu backend)
-  const login = (userData) => {
-    // Lógica de autenticación (por ejemplo, enviar datos al servidor y recibir respuesta)
-    // Si la autenticación es exitosa, establece el usuario
-    setUser(userData);
-  };
+    // Función para iniciar sesión (puedes modificarla según tu backend)
+    const login = (userData) => {
+        // Lógica de autenticación (por ejemplo, enviar datos al servidor y recibir respuesta)
+        // Si la autenticación es exitosa, establece el usuario
+        setUser(userData);
+    };
 
-  // Función para cerrar sesión
-  const logout = () => {
-    // Lógica para cerrar sesión (por ejemplo, eliminar datos de sesión del servidor)
-    // Luego, establece el usuario como null
-    setUser(null);
-  };
+    // Función para cerrar sesión
+    const logout = () => {
+        // Lógica para cerrar sesión (por ejemplo, eliminar datos de sesión del servidor)
+        // Luego, establece el usuario como null
+        setUser(null);
+        console.log(setUser);
+    };
 
-  return (
-    <AuthContext.Provider value={{ user, login, logout }}>
-      {children}
-    </AuthContext.Provider>
-  );
-};
+    return (
+        <AuthContext.Provider value={{ user, login, logout }}>
+        {children}
+        </AuthContext.Provider>
+    );
+    };
 
-export const useAuth = () => useContext(AuthContext);
+    export const useAuth = () => useContext(AuthContext);
