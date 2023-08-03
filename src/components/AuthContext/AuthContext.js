a779eb1eaafa70b6e68893aa20f61e3642972bd1
Adrian Calabro <calabroadrian@gmail.com>
2023-08-03 15:05:51 -0300

    form-general16

--- src/components/AuthContext/AuthContext.js
+++ src/components/AuthContext/AuthContext.js
@@ -3,22 +3,23 @@
     const AuthContext = createContext();
 
     export const AuthProvider = ({ children }) => {
-    const [user, setUser] = useState(null);
-
-    // Función para iniciar sesión (puedes modificarla según tu backend)
-    const login = (userData) => {
-        // Lógica de autenticación (por ejemplo, enviar datos al servidor y recibir respuesta)
-        // Si la autenticación es exitosa, establece el usuario
-        setUser(userData);
-    };
-
-    // Función para cerrar sesión
-    const logout = () => {
-        // Lógica para cerrar sesión (por ejemplo, eliminar datos de sesión del servidor)
-        // Luego, establece el usuario como null
-        setUser(null);
-        console.log(setUser);
-    };
+        const [user, setUser] = useState(null);
+      
+        // Función para iniciar sesión (puedes modificarla según tu backend)
+        const login = (userData) => {
+          // Lógica de autenticación (por ejemplo, enviar datos al servidor y recibir respuesta)
+          // Si la autenticación es exitosa, establece el usuario
+          setUser(userData);
+        };
+      
+        // Función para cerrar sesión
+        const logout = () => {
+          // Lógica para cerrar sesión (por ejemplo, eliminar datos de sesión del servidor)
+          // Luego, establece el usuario como null
+          setUser(null);
+          // Eliminar los datos del usuario almacenados en localStorage
+          localStorage.removeItem('user');
+        };
 
     return (
         <AuthContext.Provider value={{ user, login, logout }}>
