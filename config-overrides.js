f9e4ab096c64e9594322cbce6b56f6d0bdf80dfb
Adrian Calabro <adrian.calabro@globallogic.com>
2025-03-27 14:05:54 -0300

    Update: Ensure react and react-dom versions are correct

--- config-overrides.js
+++ config-overrides.js
@@ -1,15 +1,22 @@
 const webpack = require('webpack');
+const path = require('path');
 
 module.exports = function override(config, env) {
   config.resolve = {
     ...config.resolve,
     fallback: {
       path: require.resolve('path-browserify'),
+      fs: false,
+      net: false,
+      tls: false,
+      child_process: false,
+      querystring: require.resolve('querystring-es3'),
       http: require.resolve('stream-http'),
       https: require.resolve('https-browserify'),
       crypto: require.resolve('crypto-browserify'),
       stream: require.resolve('stream-browserify'),
-      vm: require.resolve('vm-browserify'), // Agregar el fallback para 'vm'
+      zlib: require.resolve('browserify-zlib'),
+      'https-proxy-agent': require.resolve('https-proxy-agent'),
     },
   };
 
