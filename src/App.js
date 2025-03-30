668f48fd9d49201307ff14ecbd60d27949fef5f7
Adrian Calabro <adrian.calabro@globallogic.com>
2025-03-30 19:58:32 -0300

    Commit render RoadmapContainer

--- src/App.js
+++ src/App.js
@@ -4,20 +4,14 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Re
 import { AuthProvider, useAuth } from './components/AuthContext/AuthContext';
 import Login from './components/Login/Login';
 import LinkedInAuthCallback from './components/LinkedInAuthCallback/LinkedInAuthCallback';
-import RoadmapDataSheet from './components/Roadmap/RoadmapDataSheet';
 import Modal from './components/Modal/Modal';
-import Form from './components/Form/Form';
 import SprintForm from './components/SprintForm/SprintForm';
 import RoadmapContainer from './components/RoadmapContainer/RoadmapContainer';
 import './App.css';
 
 function App() {
   const { user, login, logout, loginWithLinkedIn } = useAuth();
-  const [items, setItems] = useState([]);
-  const [selectedItem, setSelectedItem] = useState(null);
-  const [isModalOpen, setIsModalOpen] = useState(false);
   const [isSprintFormOpen, setIsSprintFormOpen] = useState(false);
-  const [isAddingItem, setIsAddingItem] = useState(false);
 
   useEffect(() => {
     const token = localStorage.getItem('user'); // Busca el token en localStorage
@@ -46,33 +40,6 @@ function App() {
     return <Login onLogin={loginWithLinkedIn} />;
   }
 
-  const handleAddItem = () => {
-    setIsAddingItem(true);
-    setSelectedItem(null);
-    setIsModalOpen(true);
-  };
-
-  const handleSelectItem = (item) => {
-    setIsAddingItem(false);
-    setSelectedItem(item);
-    setIsModalOpen(true);
-  };
-
-  const handleDeselectItem = () => {
-    setSelectedItem(null);
-  };
-
-  const handleUpdateItem = (updatedItem) => {
-    const updatedItems = items.map((item) =>
-      item.id === updatedItem.id ? updatedItem : item
-    );
-    setItems(updatedItems);
-  };
-
-  const handleDeleteItem = (itemToDelete) => {
-    const updatedItems = items.filter((item) => item.id !== itemToDelete.id);
-    setItems(updatedItems);
-  };
 
   const handleOpenSprintForm = () => {
     setIsSprintFormOpen(true);
@@ -89,32 +56,13 @@ function App() {
       </header>
 
       <div className="functions-container">
-        <button className="app-add-button" onClick={handleAddItem}>
-          Agregar
-        </button>
         <button className="app-add-button" onClick={handleOpenSprintForm}>
           Sprint
         </button>
       </div>
 
-      <RoadmapContainer
-        items={items}
-        onSelectItem={handleSelectItem}
-        onDeselectItem={handleDeselectItem}
-        onEditItem={handleSelectItem}
-      />
-
-      <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
-        <Form
-          item={selectedItem}
-          onAddItem={handleAddItem}
-          onDeselectItem={handleDeselectItem}
-          onUpdateItem={handleUpdateItem}
-          onDeleteItem={handleDeleteItem}
-          onCloseModal={() => setIsModalOpen(false)}          
-          isAddingItem={isAddingItem}
-        />
-      </Modal>
+      <RoadmapContainer/>
+      
       {isSprintFormOpen && (
         <Modal isOpen={isSprintFormOpen} onClose={() => setIsSprintFormOpen(false)}>
           <SprintForm onCloseModal={() => setIsSprintFormOpen(false)} />
