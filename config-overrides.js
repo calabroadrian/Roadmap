d8c9ea7208663a3d04bdaabd95cb34a2c9e95169
Adrian Calabro <adrian.calabro@globallogic.com>
2025-03-31 16:22:44 -0300

    Commit GSTCTimeline

--- config-overrides.js
+++ config-overrides.js
@@ -31,36 +31,3 @@ module.exports = function override(config, env) {
 
   return config;
 };
-const webpack = require('webpack');
-const path = require('path');
-
-module.exports = function override(config, env) {
-  config.resolve = {
-    ...config.resolve,
-    fallback: {
-      path: require.resolve('path-browserify'),
-      fs: false,
-      net: false,
-      tls: false,
-      child_process: false,
-      querystring: require.resolve('querystring-es3'),
-      http: require.resolve('stream-http'),
-      https: require.resolve('https-browserify'),
-      crypto: require.resolve('crypto-browserify'),
-      stream: require.resolve('stream-browserify'),
-      zlib: require.resolve('browserify-zlib'),
-      'https-proxy-agent': require.resolve('https-proxy-agent'),
-      vm: require.resolve('vm-browserify'), // Polyfill para 'vm'
-      process: require.resolve('process/browser.js')  // Actualizado con extensión .js
-    },
-  };
-
-  config.plugins.push(
-    new webpack.ProvidePlugin({
-      process: 'process/browser.js', // Actualizado con extensión .js
-      Buffer: ['buffer', 'Buffer'],
-    })
-  );
-
-  return config;
-};
