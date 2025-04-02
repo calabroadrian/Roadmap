e3b33c9db0a821ba7ab563a31efe0c0b0f270e96
Adrian Calabro <adrian.calabro@globallogic.com>
2025-04-02 16:16:35 -0300

    commit

--- src/components/MyTimeline.js
+++ src/components/MyTimeline.js
@@ -1,7 +1,7 @@
 // src/components/MyTimeline.js
 import React from "react";
 import Timeline from "react-calendar-timeline";
-import "./MyTimeline.css";
+import "./MyTimeline.css"
 import moment from "moment";
 
 const MyTimeline = ({ tasks }) => {
